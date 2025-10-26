780388505aa9ec43ca74cfcee3e22f35df01d5ca
tldjfj123 <dksckd0804@naver.com>
2025-10-26 16:15:34 +0900

    feat : Implement Game class

---
+++ src/main/java/racingcar/Game.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import java.util.List;
+
+/**
+ * 게임 진행에 필요한 메소드들을 모은 class
+ */
+public class Game {
+
+    public Game() {
+    }
+
+    // 플레이어 등록 메소드
+    public List<Player> registerPlayers() {
+        // 구현 예정
+        return null;
+    }
+
+    // 실행 횟수 등록 메소드
+    public int registerNumberOfAttempts() {
+        // 구현 예정
+        return 0;
+    }
+
+    // 게임 실행 메소드
+    public void playGame(List<Player> players) {
+        // 구현 예정
+    }
+
+    // 현재 상황 출력 메소드
+    public void printGameResult(List<Player> players) {
+        // 구현 예정
+    }
+
+    // 승자 출력 메소드
+    public void printWinner(List<Player> players) {
+        // 구현 예정
+    }
+}
