612e56191ba8a9c1dd384ce877d554191eb3e367
tldjfj123 <dksckd0804@naver.com>
2025-10-26 21:38:07 +0900

    feat : Implement Application class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+
+        List<Player> players = game.registerPlayers();
+        int numberOfAttempts = game.registerNumberOfAttempts();
+
+        for (int i = 0; i < numberOfAttempts; i++) {
+            game.playGame(players);
+            game.printGameResult(players);
+        }
+
+        game.printWinner(players);
     }
 }
