df424331c556edd5eb4ad059e20c65f5d234145e
tldjfj123 <dksckd0804@naver.com>
2025-10-26 13:42:03 +0900

    feat : Implement Player class

---
+++ src/main/java/racingcar/Player.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class Player {
+    String name;
+    int position;
+
+    public Player(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+}
